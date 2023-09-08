3f429fcc8c74da10b1f1ac22c7360d0cca7946cb
Hanka <hrosicka@post.cz>
2023-09-08 19:28:22 +0200

    Záměna znaku - funkce

---
+++ ZamenZnak.py
@@ -0,0 +1,36 @@
+def zamen(slovo, pozice, novyZnak):
+    """
+        Funkce pro záměnu znaku ve slově
+
+        Parametry:
+
+        slovo - původní slovo
+
+        pozice - pozice, na které se zaměňuje znak (počítáno od 1)
+
+        novyZnak - znak, který bude použit místo původního
+
+        """
+    noveSlovo = slovo[:pozice] + novyZnak + slovo[(pozice + 1):]
+    return noveSlovo
+       
+print("MĚNIČ PÍSMEN")
+print("------------")
+
+slovo = input('Slovo: ')
+
+while True:
+            pozStr = input('Které písmeno zaměnit (počítá se od jedničky)? ')
+            try:
+                pozice = int(pozStr) - 1
+                break
+            except ValueError:
+                print("Zadaná hodnota není číslo! Zkus to znovu", end = "!!\n") 
+
+novyZnak = input('Nové písmeno (nebo skupina písmen): ')
+
+noveSlovo = zamen(slovo, pozice, novyZnak)
+
+print("Slovo {} se změnilo na slovo {}.".format(slovo.upper(), noveSlovo.upper()))
+
+
