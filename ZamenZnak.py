631e4573e366c20d0e9885695c87a35adda84ecb
Hanka <hrosicka@post.cz>
2023-09-09 09:47:51 +0200

    Záměna znaků pomocí funkcí

--- ZamenZnak.py
+++ ZamenZnak.py
@@ -10,24 +10,50 @@ def zamen(slovo, pozice, novyZnak):
 
         novyZnak - znak, který bude použit místo původního
 
+        Return:
+
+        nove slovo, kde je na dané pozici zaměněn znak
+
         """
     noveSlovo = slovo[:pozice] + novyZnak + slovo[(pozice + 1):]
     return noveSlovo
+
+
+def nacti():
+    """
+        Funkce načte slovo, pořadí znaku pro výměnu a nový znak
+
+        Return:
+
+        tuple
+
+        slovo - původní slovo
+
+        pozice - pozice, na které měníme
+
+        novyZnak - znak nebo skupina znaků, který se použijí místo původního
+        """
+    slovo = input('Slovo: ')
+
+    while True:
+                pozStr = input('Které písmeno zaměnit (počítá se od jedničky)? ')
+                try:
+                    pozice = int(pozStr) - 1
+                    break
+                except ValueError:
+                    print("Zadaná hodnota není číslo! Zkus to znovu", end = "!!\n") 
+
+    novyZnak = input('Nové písmeno (nebo skupina písmen): ')
+    return (slovo, pozice, novyZnak)
+      
        
 print("MĚNIČ PÍSMEN - FUNKCE")
 print("---------------------")
 
-slovo = input('Slovo: ')
-
-while True:
-            pozStr = input('Které písmeno zaměnit (počítá se od jedničky)? ')
-            try:
-                pozice = int(pozStr) - 1
-                break
-            except ValueError:
-                print("Zadaná hodnota není číslo! Zkus to znovu", end = "!!\n") 
-
-novyZnak = input('Nové písmeno (nebo skupina písmen): ')
+zamena1 = nacti()
+slovo = zamena1[0]
+pozice = zamena1[1]
+novyZnak = zamena1[2]
 
 noveSlovo = zamen(slovo, pozice, novyZnak)
 
